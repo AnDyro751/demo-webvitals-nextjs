adab066fab901db3ae7547a290bbb33f2518def0
Angel Mendez <angelmendezz751@gmail.com>
2020-07-30 23:06:58 -0500

    REPORT

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export function reportWebVitals(metric) {
+	console.log(metric)
+}
+
+function MyApp({Component, pageProps}) {
+	return <Component {...pageProps} />
 }
 
 export default MyApp
